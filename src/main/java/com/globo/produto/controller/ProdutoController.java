9c32dd32f0623104a40a8b4af21a107f43a1c8dc
adolfobjj <luizadolfo7@hotmail.com>
2023-10-16 08:48:26 -0300

    Correção do Restcontroller

--- src/main/java/com/globo/produto/controller/ProdutoController.java
+++ src/main/java/com/globo/produto/controller/ProdutoController.java
@@ -1,5 +1,7 @@
 package com.globo.produto.controller;
 
+import com.globo.produto.entity.Produto;
+import com.globo.produto.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
