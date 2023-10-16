7b60d835980edac3b21c312a161c275f6ae2c6f3
adolfobjj <luizadolfo7@hotmail.com>
2023-10-16 08:48:47 -0300

    Correção do Repository

--- src/main/java/com/globo/produto/repository/ProdutoRepository.java
+++ src/main/java/com/globo/produto/repository/ProdutoRepository.java
@@ -1,5 +1,6 @@
 package com.globo.produto.repository;
 
+import com.globo.produto.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
