402481647da38ce4986b1bc25d9e790b1b23b903
adolfobjj <luizadolfo7@hotmail.com>
2023-10-16 08:44:02 -0300

    Criação do Restcontroller

---
+++ src/main/java/com/globo/produto/controller/ProdutoController.java
@@ -0,0 +1,48 @@
+package com.globo.produto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> obterTodosProdutos() {
+        List<Produto> produtos = produtoService.obterTodos();
+        return new ResponseEntity<>(produtos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> obterProdutoPorId(@PathVariable Long id) {
+        Optional<Produto> produto = produtoService.obterPorId(id);
+        return produto.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criarProduto(@RequestBody Produto produto) {
+        Produto novoProduto = produtoService.criarProduto(produto);
+        return new ResponseEntity<>(novoProduto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody Produto produto) {
+        Produto produtoAtualizado = produtoService.atualizarProduto(id, produto);
+        return new ResponseEntity<>(produtoAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirProduto(@PathVariable Long id) {
+        produtoService.excluirProduto(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
