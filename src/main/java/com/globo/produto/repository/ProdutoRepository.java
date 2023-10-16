8e868b161a5c962054d12eb0df71b6df237c304b
adolfobjj <luizadolfo7@hotmail.com>
2023-10-16 08:42:59 -0300

    Criação do repository

---
+++ src/main/java/com/globo/produto/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.globo.produto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
